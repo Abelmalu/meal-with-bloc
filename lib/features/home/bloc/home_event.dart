903638a58693e7a8c84107dcbe93d606f58bf3cb
abel malu <abellmalu@gmail.com>
2024-12-27 10:19:48 +0300

    first commit

---
+++ lib/features/home/bloc/home_event.dart
@@ -0,0 +1,16 @@
+part of 'home_bloc.dart';
+
+@immutable
+sealed class HomeEvent {}
+
+
+final class HomeCartButtonEvent extends HomeEvent{
+
+
+
+}
+
+final class HomeCartButtonNavigateEvent extends HomeEvent{
+
+
+}
